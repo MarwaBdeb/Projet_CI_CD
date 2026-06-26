fccb2c730a288e10db044de8d9398026275fc0bb
MarwaBdeb <6397314@bdeb.qc.ca>
2026-06-26 11:38:21 -0400

    correction du bug2

--- tests/test_connexion.py
+++ tests/test_connexion.py
@@ -3,6 +3,8 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def driver():
@@ -24,17 +26,21 @@ def driver():
         pass # Évite de faire planter le teardown si le test a échoué avant
     browser.quit()
 
-def test_login(driver):
+def test_login(driver,browser):
 # AJUSTEMENT CLOUD 3 : Remplacement par une IP publique ou variable d'environnement
 # Si APP_URL n'est pas fournie par GitHub, on utilise l'IP publique de votre Redmine
 
     url = os.environ.get("APP_URL", "http://34.123.38.242")
     username = os.environ.get("APP_USERNAME", "user1")
     password = os.environ.get("APP_PASSWORD", "Hdjsy@1hrts")
+    
+
     driver.get(url + "/login")
     driver.find_element(By.ID, "username").send_keys(username)
     driver.find_element(By.ID, "password").send_keys(password)
     driver.find_element(By.ID, "login-submit").click()
+    wait = WebDriverWait(browser, 10)
+    wait.until(EC.visibility_of_element_located((By.ID, "loggedas")))
 
     # Critère de succès
     loggedas = driver.find_element(By.ID, "loggedas")
